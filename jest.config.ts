42edd00b9e31ed35d0ff519c87dd1a87ce70f045
Wayne Van Son <waynevanson@gmail.com>
2023-08-08 08:17:28 +1000

    chore: move configs to packagejson

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-
-import { Config } from 'jest';
-
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  passWithNoTests: true,
-} as Config;
