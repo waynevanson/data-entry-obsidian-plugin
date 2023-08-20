8636f15b2a4d89ee838a2220146e4e5392407fd6
Wayne Van Son <waynevanson@gmail.com>
2023-08-21 08:50:59 +1000

    feat: polyfill react with preact

--- packages/data-entry/esbuild.config.mjs
+++ packages/data-entry/esbuild.config.mjs
@@ -16,6 +16,12 @@ const context = await esbuild.context({
   },
   entryPoints: ['main.ts'],
   bundle: true,
+  alias: {
+    react: 'preact/compat',
+    'react-dom/test-utils': 'preact/test-utils',
+    'react-dom': 'preact/compat',
+    'react/jsx-runtime': 'preact/jsx-runtime',
+  },
   external: [
     'obsidian',
     'electron',
