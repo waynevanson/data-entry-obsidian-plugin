5ee446cb72dcaa26595184a8653edf3c3a5723a6
Wayne Van Son <waynevanson@gmail.com>
2023-08-21 08:11:02 +1000

    test(use-file): add file does not exist test

--- packages/data-entry/src/hooks/datasource/file.spec.ts
+++ packages/data-entry/src/hooks/datasource/file.spec.ts
@@ -34,4 +34,20 @@ describe(useFile, () => {
       expect(hooked.result.current.data).toBeNull();
     });
   });
+
+  it('should be show an error when the file does not exist', async () => {
+    const file = {
+      path: 'file.md',
+      contents: 'Hello, World!',
+    };
+    const mocks = createMocks();
+    const hooked = rtl.renderHook(() => useFile(mocks.vault, file.path));
+    await rtl.waitFor(() => {
+      expect(hooked.result.current.error).toBeInstanceOf(FileNotFoundError);
+      expect(hooked.result.current.data).toBeNull();
+    });
+  });
+
+  it.todo('NotTFile');
+  it.todo('FileRenamed');
 });
