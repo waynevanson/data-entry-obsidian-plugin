f99e6769c7ae0e8b4898e64ccd751619fa022324
Wayne Van Son <waynevanson@gmail.com>
2023-08-21 08:08:12 +1000

    test(use-file): add file not found

--- packages/data-entry/src/hooks/datasource/file.spec.ts
+++ packages/data-entry/src/hooks/datasource/file.spec.ts
@@ -1,9 +1,8 @@
 import { rtl } from '../../test-utils';
 import { createMocks } from '../../test-utils/mocks';
-import { useFile } from './file';
+import { useFile, FileNotFoundError } from './file';
 
 describe(useFile, () => {
-  // todo-fix with actual contents
   it('should be able to read the file from the vault', async () => {
     const file = {
       path: 'file.md',
@@ -20,6 +19,19 @@ describe(useFile, () => {
     await rtl.waitFor(() => {
       expect(hooked.result.current.data).toBe(file.contents);
       expect(hooked.result.current.error).toBeNull();
+    });
+  });
+
+  it('should be show an error when the file does not exist', async () => {
+    const file = {
+      path: 'file.md',
+      contents: 'Hello, World!',
+    };
+    const mocks = createMocks();
+    const hooked = rtl.renderHook(() => useFile(mocks.vault, file.path));
+    await rtl.waitFor(() => {
+      expect(hooked.result.current.error).toBeInstanceOf(FileNotFoundError);
+      expect(hooked.result.current.data).toBeNull();
     });
   });
 });
