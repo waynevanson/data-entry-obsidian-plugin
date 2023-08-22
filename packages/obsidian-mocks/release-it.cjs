00597cab255c6aa806106ade7fbb67332fb84423
Wayne Van Son <waynevanson@gmail.com>
2023-08-22 22:08:13 +1000

    chore(ci): migrate to turbo [skip ci]

---
+++ packages/obsidian-mocks/release-it.cjs
@@ -0,0 +1,32 @@
+const version = '${version}';
+const packageName = process.env.npm_package_name;
+const scope = packageName.split('/')[1];
+
+module.exports = {
+  plugins: {
+    '@release-it/conventional-changelog': {
+      path: '.',
+      infile: 'CHANGELOG.md',
+      preset: 'conventionalcommits',
+      gitRawCommitsOpts: {
+        path: '.',
+      },
+    },
+  },
+  git: {
+    push: true,
+    tagName: `${packageName}-${version}`,
+    pushRepo: 'git@github.com:waynevanson/data-entry-obsidian-plugin.git',
+    commitsPath: '.',
+    commitMessage: `feat(${scope}): released version ${version} [no ci]`,
+    requireCommits: true,
+    requireCommitsFail: false,
+  },
+  npm: {
+    publish: false,
+  },
+  github: {
+    release: true,
+    releaseName: `${packageName}-${version}`,
+  },
+};
