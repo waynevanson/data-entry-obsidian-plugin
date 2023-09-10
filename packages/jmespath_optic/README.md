9c87f5d70a15589b5adde8262cb10a2412edbfd2
Wayne Van Son <waynevanson@gmail.com>
2023-09-10 18:28:12 +1000

    refactor: support jsme index

---
+++ packages/jmespath_optic/README.md
@@ -0,0 +1,3 @@
+# jmespath_optic
+
+Functional optics that change JSON values using the provided JMESPATH expression.
--- packages/jmespath_optic/src/lib.rs
+++ packages/jmespath_optic/src/lib.rs
@@ -2,14 +2,25 @@ use jmespath::ast::Ast;
 use serde_json::Value;
 
 pub trait JmesTraversal {
-    fn modify_json_value(&self, source: Value, target: Value) -> Value;
+    fn modify_json_value(&self, source: Value, target: Value) -> Option<Value>;
 }
 
 impl JmesTraversal for &Ast {
-    fn modify_json_value(&self, source: Value, target: Value) -> Value {
+    fn modify_json_value(&self, source: Value, target: Value) -> Option<Value> {
         match self {
-            Ast::Identity { .. } => target,
-            Ast::And { lhs, rhs, .. } => target,
+            Ast::Identity { .. } => target.into(),
+            Ast::Index { idx, .. } => {
+                // todo - take from source, not by cloning
+                let mut array = source.as_array()?.to_owned();
+                let index = if *idx >= 0 {
+                    *idx
+                } else {
+                    *idx + (array.len() as i32)
+                };
+                let item = array.get_mut(index as usize)?;
+                *item = target;
+                Some(array.into())
+            }
             _ => todo!(),
         }
     }
@@ -18,16 +29,28 @@ impl JmesTraversal for &Ast {
 #[cfg(test)]
 mod test {
     use super::*;
+    use serde_json::{json, Value};
 
     #[test]
     fn identity() {
         let expression = jmespath::compile("@").unwrap();
         let ast = expression.as_ast();
-        assert_eq!(ast, &Ast::Identity { offset: 0 });
 
-        let source: Value = serde_json::from_str(r#"{ "hello": "world" }"#).unwrap();
-        let target = source.clone();
-        let result: Value = ast.modify_json_value(source, target.clone());
+        let source = json!({ "hello": "world"});
+        let target = json!("my man");
+        let result: Value = ast.modify_json_value(source, target.clone()).unwrap();
         assert_eq!(result, target);
+    }
+
+    #[test]
+    fn index() {
+        let expression = jmespath::compile("[0]").unwrap();
+        let ast = expression.as_ast();
+
+        let source = json!(["world"]);
+        let target = json!("sup");
+        let expected = json!([target]);
+        let result: Value = ast.modify_json_value(source, target.clone()).unwrap();
+        assert_eq!(result, expected);
     }
 }
