25edc754bf97be4998a0a4100274ebe8809b8795
Wayne Van Son <waynevanson@gmail.com>
2023-09-12 17:00:06 +1000

    test: allow recursion through boxes

--- packages/jmespath_optic/src/lib.rs
+++ packages/jmespath_optic/src/lib.rs
@@ -1,217 +1,259 @@
-use jmespath::{ast::Ast, interpreter::interpret, parse, Context, Runtime};
+use jmespath::{ast::Ast, interpreter::interpret, Context, DEFAULT_RUNTIME};
 use serde_json::Value;
 use std::rc::Rc;
 
-pub struct Traversal<'context> {
+pub fn modify_option_kleisli(
     ast: Ast,
-    context: Context<'context>,
-    expression: String,
+    mut context: Context,
+    expression: &str,
+    source: Value,
+    modify: Box<&dyn Fn(Value) -> Option<Value>>,
+) -> Option<Value> {
+    match ast {
+        Ast::Identity { .. } => modify(source),
+        Ast::Index { idx, .. } => {
+            let mut array = source.as_array()?.to_owned();
+            let index = if idx >= 0 {
+                idx
+            } else {
+                idx + (array.len() as i32)
+            };
+            let target = array.get(index as usize)?.to_owned();
+            let element = array.get_mut(index as usize)?;
+            *element = modify(target)?;
+            Some(array.into())
+        }
+        Ast::Field { name, .. } => {
+            let mut object = source.as_object()?.to_owned();
+            let target = object.get(&name)?.to_owned();
+            let element = object.get_mut(&name)?;
+            *element = modify(target)?;
+            Some(object.into())
+        }
+        Ast::And { lhs, rhs, .. } => {
+            let data = Rc::new(source.clone().try_into().ok()?);
+            let interpretted = interpret(&data, &lhs, &mut context).ok()?;
+            let ast = *if !interpretted.is_truthy() { lhs } else { rhs };
+
+            modify_option_kleisli(ast, context, expression, source, modify)
+        }
+        Ast::Or { lhs, rhs, .. } => {
+            let data = Rc::new(source.clone().try_into().ok()?);
+            let interpretted = interpret(&data, &lhs, &mut context).ok()?;
+            let ast = *if interpretted.is_truthy() {
+                Some(lhs)
+            } else if interpret(&data, &rhs, &mut context).ok()?.is_truthy() {
+                Some(rhs)
+            } else {
+                None
+            }?;
+            modify_option_kleisli(ast, context, expression, source, modify)
+        }
+        Ast::Subexpr { lhs, rhs, .. } => {
+            let closure = |target: Value| {
+                modify_option_kleisli(
+                    *rhs.clone(),
+                    Context::new(expression, &DEFAULT_RUNTIME),
+                    expression,
+                    target,
+                    Box::new(modify.as_ref()),
+                )
+            };
+            modify_option_kleisli(*lhs, context, expression, source, Box::new(&closure))
+        }
+        Ast::Comparison { .. } => unimplemented!(),
+        _ => todo!(),
+    }
 }
 
-impl<'context> Traversal<'context> {
-    pub fn new(expression: &'context str, runtime: &'context Runtime) -> Self {
-        let context = Context::new(expression, runtime);
-        let ast = parse(expression).unwrap();
-
-        Self {
-            ast,
-            context,
-            expression: expression.to_string(),
-        }
-    }
-
-    pub fn from_ast(&'context self, ast: Ast) -> Self {
-        Self {
-            ast,
-            context: Context::new(&self.expression, self.context.runtime),
-            expression: self.expression.to_owned(),
-        }
-    }
-
-    pub fn modify_option_kleisli(
-        mut self,
-        source: Value,
-        modify: impl Fn(Value) -> Option<Value>,
-    ) -> Option<Value> {
-        match self.ast {
-            Ast::Identity { .. } => modify(source),
-            Ast::Index { idx, .. } => {
-                let mut array = source.as_array()?.to_owned();
-                let index = if idx >= 0 {
-                    idx
-                } else {
-                    idx + (array.len() as i32)
-                };
-                let target = array.get(index as usize)?.to_owned();
-                let element = array.get_mut(index as usize)?;
-                *element = modify(target)?;
-                Some(array.into())
-            }
-            Ast::Field { name, .. } => {
-                let mut object = source.as_object()?.to_owned();
-                let target = object.get(&name)?.to_owned();
-                let element = object.get_mut(&name)?;
-                *element = modify(target)?;
-                Some(object.into())
-            }
-            Ast::And { lhs, rhs, .. } => {
-                let data = Rc::new(source.clone().try_into().ok()?);
-                let interpretted = interpret(&data, &lhs, &mut self.context).ok()?;
-                let ast = *if !interpretted.is_truthy() { lhs } else { rhs };
-                Traversal {
-                    ast,
-                    context: self.context,
-                    expression: self.expression,
-                }
-                .modify_option_kleisli(source, modify)
-            }
-            Ast::Or { lhs, rhs, .. } => {
-                let data = Rc::new(source.clone().try_into().ok()?);
-                let interpretted = interpret(&data, &lhs, &mut self.context).ok()?;
-                let ast = *if interpretted.is_truthy() {
-                    Some(lhs)
-                } else if interpret(&data, &rhs, &mut self.context).ok()?.is_truthy() {
-                    Some(rhs)
-                } else {
-                    None
-                }?;
-                Traversal {
-                    ast,
-                    context: self.context,
-                    expression: self.expression,
-                }
-                .modify_option_kleisli(source, modify)
-            }
-            Ast::Comparison { .. } => unimplemented!(),
-            _ => todo!(),
-        }
-    }
-
-    pub fn set(self, source: Value, target: Value) -> Value {
-        self.set_option(source.clone(), target).unwrap_or(source)
-    }
-
-    pub fn set_option(self, source: Value, target: Value) -> Option<Value> {
-        self.modify_option_kleisli(source, |_| Some(target.clone()))
-    }
-
-    pub fn modify(self, source: Value, modify: impl Fn(Value) -> Value) -> Value {
-        self.modify_option_kleisli(source.clone(), |target| Some(modify(target)))
-            .unwrap_or(source)
-    }
-}
 #[cfg(test)]
 mod test {
     use super::*;
+    use jmespath::{parse, Runtime, DEFAULT_RUNTIME};
     use serde_json::{json, Value};
 
     #[test]
     fn identity() {
-        let runtime = Runtime::new();
-        let traversal = Traversal::new("@", &runtime);
+        let runtime = &DEFAULT_RUNTIME;
+        let expression = "@";
+        let ast = parse(expression).unwrap();
+        let context = Context::new(expression, &runtime);
 
         let source = json!({ "hello": "world"});
         let target = json!("my man");
         let expected = target.clone();
-
-        let result: Value = traversal.set_option(source, target).unwrap();
+        let m = move |_: Value| Some(target.clone());
+        let result: Value =
+            modify_option_kleisli(ast, context, expression, source, Box::new(&m)).unwrap();
         assert_eq!(result, expected);
     }
 
-    // todo - negative indexes
     #[test]
     fn index_positive() {
         let runtime = Runtime::new();
-        let traversal = Traversal::new("[0]", &runtime);
+        let expression = "[0]";
+        let ast = parse(expression).unwrap();
+        let context = Context::new(expression, &runtime);
 
         let source = json!(["world"]);
         let target = json!("sup");
         let expected = json!([target]);
 
-        let result: Value = traversal.set_option(source, target.clone()).unwrap();
+        let result: Value = modify_option_kleisli(
+            ast,
+            context,
+            expression,
+            source,
+            Box::new(&move |_| Some(target.clone())),
+        )
+        .unwrap();
         assert_eq!(result, expected);
     }
 
     #[test]
     fn index_negative() {
         let runtime = Runtime::new();
-        let traversal = Traversal::new("[-1]", &runtime);
+        let expression = "[-1]";
+        let ast = parse(expression).unwrap();
+        let context = Context::new(expression, &runtime);
 
         let source = json!(["world", "earth", "globe"]);
         let target = json!("sup");
         let expected = json!(["world", "earth", target]);
-        let result: Value = traversal.set_option(source, target.clone()).unwrap();
+        let result: Value = modify_option_kleisli(
+            ast,
+            context,
+            expression,
+            source,
+            Box::new(&|_| Some(target.clone())),
+        )
+        .unwrap();
         assert_eq!(result, expected);
     }
 
     #[test]
     fn field() {
         let runtime = Runtime::new();
-        let traversal = Traversal::new("hello", &runtime);
+        let expression = "hello";
+        let ast = parse(expression).unwrap();
+        let context = Context::new(expression, &runtime);
 
         let source = json!({ "hello": "world" });
         let target = json!("sup");
         let expected = json!({ "hello": target });
-        let result: Value = traversal.set_option(source, target.clone()).unwrap();
+        let result: Value = modify_option_kleisli(
+            ast,
+            context,
+            expression,
+            source,
+            Box::new(&|_| Some(target.clone())),
+        )
+        .unwrap();
         assert_eq!(result, expected);
     }
 
     #[test]
     fn and_left() {
         let runtime = Runtime::new();
-        let traversal = Traversal::new(r#"hello && goodbye"#, &runtime);
+        let expression = "hello && goobye";
+        let ast = parse(expression).unwrap();
+        let context = Context::new(expression, &runtime);
 
         let source = json!({ "hello": "", "goodbye": "earth" });
         let target = json!("sup");
         let expected = json!({ "hello": target, "goodbye": "earth"});
-        let result: Value = traversal.set_option(source, target.clone()).unwrap();
+        let result: Value = modify_option_kleisli(
+            ast,
+            context,
+            expression,
+            source,
+            Box::new(&|_| Some(target.clone())),
+        )
+        .unwrap();
         assert_eq!(result, expected);
     }
 
     #[test]
     fn and_right() {
         let runtime = Runtime::new();
-        let traversal = Traversal::new(r#"hello && goodbye"#, &runtime);
+        let expression = "hello && goodbye";
+        let ast = parse(expression).unwrap();
+        let context = Context::new(expression, &runtime);
 
         let source = json!({ "hello": "world", "goodbye": "earth" });
         let target = json!("sup");
-        let expected = json!({ "hello": "world", "goodbye": target });
-        let result: Value = traversal.set_option(source, target.clone()).unwrap();
+        let expected = json!({ "hello": "world", "goodbye": target});
+        let result: Value = modify_option_kleisli(
+            ast,
+            context,
+            expression,
+            source,
+            Box::new(&|_| Some(target.clone())),
+        )
+        .unwrap();
         assert_eq!(result, expected);
     }
 
     #[test]
     fn or_left() {
         let runtime = Runtime::new();
-        let traversal = Traversal::new(r#"hello || goodbye"#, &runtime);
+        let expression = "hello || goodbye";
+        let ast = parse(expression).unwrap();
+        let context = Context::new(expression, &runtime);
 
         let source = json!({ "hello": "", "goodbye": "earth" });
         let target = json!("sup");
         let expected = json!({ "hello": "", "goodbye": target});
-        let result: Value = traversal.set_option(source, target.clone()).unwrap();
+        let result: Value = modify_option_kleisli(
+            ast,
+            context,
+            expression,
+            source,
+            Box::new(&|_| Some(target.clone())),
+        )
+        .unwrap();
+
         assert_eq!(result, expected);
     }
 
     #[test]
     fn or_right() {
         let runtime = Runtime::new();
-        let traversal = Traversal::new(r#"hello || goodbye"#, &runtime);
+        let expression = "hello || goodbye";
+        let ast = parse(expression).unwrap();
+        let context = Context::new(expression, &runtime);
 
-        let source = json!({ "hello": "world", "goodbye": "earth" });
+        let source = json!({ "hello": "", "goodbye": "earth" });
         let target = json!("sup");
-        let expected = json!({ "hello": target, "goodbye": "earth" });
-        let result: Value = traversal.set_option(source, target.clone()).unwrap();
+        let expected = json!({ "hello": "", "goodbye": target });
+        let result: Value = modify_option_kleisli(
+            ast,
+            context,
+            expression,
+            source,
+            Box::new(&|_| Some(target.clone())),
+        )
+        .unwrap();
+
         assert_eq!(result, expected);
     }
 
     #[test]
     fn or_null() {
         let runtime = Runtime::new();
-        let traversal = Traversal::new(r#"hello || goodbye"#, &runtime);
+        let expression = "hello || goodbye";
+        let ast = parse(expression).unwrap();
+        let context = Context::new(expression, &runtime);
 
         let source = json!({ "hello": "", "goodbye": "" });
         let target = json!("sup");
-        let result: Option<Value> = traversal.set_option(source, target.clone());
+        let result = modify_option_kleisli(
+            ast,
+            context,
+            expression,
+            source,
+            Box::new(&|_| Some(target.clone())),
+        );
         assert_eq!(result, None);
     }
 }
